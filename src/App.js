a4462871014272dd576381d250273d62e7911656
PlaPud <patcub252545@gmail.com>
2022-09-05 01:19:11 +0700

    Delete React's starter CSS

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import CounterPage from './views/CounterPage';
 function App() {
   return (
     <div className="App">
-      {/* <h2>Navbar</h2> */}
-      
       <Routes>
         <Route path='/' element={<Homepage/>}/>
         <Route path='/counter' element={<CounterPage/>}/>
