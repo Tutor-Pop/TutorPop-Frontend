2f3d3210d06b9dc9445fb20e15faa218424067a3
PlaPud <patcub252545@gmail.com>
2022-09-06 00:30:49 +0700

    TP-64, TP-65 Done (no logo if too small)

--- src/views/Homepage.js
+++ src/views/Homepage.js
@@ -6,14 +6,34 @@ import logoWhite from '../img/logo-white.png'
 const Homepage = () => {
   return (
     <div className='homepage'>
-      <img className='app-logo' src={logoWhite} alt='tutor-pop-logo'/>
       <div className='main-container'>
-        <h1>Lorem ipsum dolor sit amet,</h1>
-        <div className='sub-text'>
-          <h2>consectetur adipiscing elit.</h2> 
-          <h2>Suspendisse vel eros vehicula,</h2>
+        <div className='text-area'>
+          <h1>Welcome to TUTOR•POP</h1>
+          <div className='sub-text'>
+            <h2>consectetur adipiscing elit.</h2> 
+            <h2>Suspendisse vel eros vehicula,</h2>
+            <Link className='link-text' to='/course'>Enter Website</Link>
+          </div>
         </div>
-        <Link className='link-text' to='/course'>Enter Website</Link>
+        <img className='app-logo' src={logoWhite} alt='tutor-pop-logo'/>
+      </div>
+      <div className='app-stat'>
+        <div className='stat-item'>
+          <p>XXXX</p>
+          <p>Users</p>
+        </div>
+        <div className='stat-item'>
+          <p>XXXX</p>
+          <p>Courses</p>
+        </div>
+        <div className='stat-item'>
+          <p>XXX</p>
+          <p>Teachers</p>
+        </div>
+        <div className='stat-item'>
+          <p>XX</p>
+          <p>Schools</p>
+        </div>
       </div>
     </div>
   )
