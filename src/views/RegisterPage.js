31cabc4492b4c1b31befddf7f48358158d60664b
Aduiduidui <thanakrit.earth@gmail.com>
2022-09-16 22:50:20 +0700

    fix form

--- src/views/RegisterPage.js
+++ src/views/RegisterPage.js
@@ -1,30 +1,34 @@
 import React, { useEffect, useState } from "react";
-import { Button, Col, Container, Form, FormGroup, Input, Label, Row } from "reactstrap";
+import {
+  Button,
+  Col,
+  Container,
+  Form,
+  FormGroup,
+  Input,
+  Label,
+  Row,
+} from "reactstrap";
 
 const RegisterPage = () => {
+  const [firstname, setFirstname] = useState("");
+  const [invalid, setInvalid] = useState({ firstname: false });
+  const [valid, setValid] = useState({ firstname: false });
 
-  const [firstname,setFirstname] = useState("")
-  const [invalid,setInvalid] = useState({firstname:false})
-  const [valid,setValid] = useState({firstname:false})
+  useEffect(() => {
+    if (firstname == "abcde") {
+      setInvalid({ firstname: true });
+      setValid({ firstname: false });
+    } else if (firstname == "") {
+      setValid({ firstname: false });
+      setInvalid({ firstname: false });
+    } else {
+      setValid({ firstname: true });
+      setInvalid({ firstname: false });
+    }
+  }, [firstname]);
 
-  useEffect(()=>{
-    if(firstname == 'abcde'){
-      setInvalid({firstname:true})
-      setValid({firstname:false})
-    }
-    else if(firstname == ''){
-      setValid({firstname:false})
-      setInvalid({firstname:false})
-    }
-    else{
-      setValid({firstname:true})
-      setInvalid({firstname:false})
-    }
-  },[firstname])
-
-  const validation = () => {
-    
-  }
+  const validation = () => {};
 
   return (
     <Container className="register">
@@ -34,11 +38,21 @@ const RegisterPage = () => {
         </Row>
         <Row xs={1} md={2}>
           <Col>
-            <Label for="firstname">Firstname<span className="required-star">*</span></Label>
-            <Input valid={valid.firstname} invalid={invalid.firstname} id="firstname" type="text" onChange={(e)=>setFirstname(e.target.value)}/>
+            <Label for="firstname">
+              Firstname<span className="required-star">*</span>
+            </Label>
+            <Input
+              valid={valid.firstname}
+              invalid={invalid.firstname}
+              id="firstname"
+              type="text"
+              onChange={(e) => setFirstname(e.target.value)}
+            />
           </Col>
           <Col>
-            <Label for="lastname">Lastname<span className="required-star">*</span></Label>
+            <Label for="lastname">
+              Lastname<span className="required-star">*</span>
+            </Label>
             <Input id="lastname" type="text" />
           </Col>
         </Row>
@@ -53,31 +67,39 @@ const RegisterPage = () => {
               <Label className="mt-3" for="Password">
                 Password<span className="required-star">*</span>
               </Label>
-              <Input required={true} className="input" id="Password" type="password" />
+              <Input
+                required={true}
+                className="input"
+                id="Password"
+                type="password"
+              />
 
               <Label className="mt-3" for="Password">
                 Confirm Password<span className="required-star">*</span>
               </Label>
-              <Input required={true} className="input" id="Password" type="password" />
+              <Input
+                required={true}
+                className="input"
+                id="Password"
+                type="password"
+              />
             </FormGroup>
           </Form>
         </Row>
         <Row md={2} xs={1}>
           <Col>
-            <Form>
-              <FormGroup>
-                <Label for="Email">Email<span className="required-star">*</span></Label>
-                <Input required={true} id="Email" type="email" />
-              </FormGroup>
-            </Form>
+            <FormGroup>
+              <Label for="Email">
+                Email<span className="required-star">*</span>
+              </Label>
+              <Input required={true} id="Email" type="email" />
+            </FormGroup>
           </Col>
           <Col>
-            <Form>
-              <FormGroup>
-                <Label for="birthyear">Birthyear</Label>
-                <Input id="birthyear" type="number" />
-              </FormGroup>
-            </Form>
+            <FormGroup>
+              <Label for="birthyear">Birthyear</Label>
+              <Input id="birthyear" type="number" />
+            </FormGroup>
           </Col>
         </Row>
         <Row>
@@ -85,16 +107,11 @@ const RegisterPage = () => {
             <Label for="description">Describe yourself</Label>
             <Input id="description" name="text" type="textarea" />
           </FormGroup>
-
         </Row>
         <Row className="justify-evenly">
-        <Button
-                  className="register-btn my-20"
-                  color="primary"
-                  size="lg"
-                >
-                  Register
-                </Button>
+          <Button className="register-btn my-20" color="primary" size="lg">
+            Register
+          </Button>
         </Row>
       </Row>
     </Container>
