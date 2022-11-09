f08331d6ac0d68b2fad1c0f1f1c6cbf87ef54dc4
KanonKC <kanonkce@gmail.com>
2022-11-10 03:33:19 +0700

    TP-201: Change password add api

--- src/views/ChangePassword.js
+++ src/views/ChangePassword.js
@@ -1,58 +1,122 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { Button, Col, Container, Form, FormGroup, Input, Label, Row } from 'reactstrap'
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import {
+    Button,
+    Col,
+    Container,
+    Form,
+    FormFeedback,
+    FormGroup,
+    Input,
+    Label,
+    Row,
+} from "reactstrap";
+import { emitError, emitSuccess } from "../functions/toastify.function";
+import { startLoading, stopLoading } from "../redux/loading.reducer";
+import { chagenPassword } from "../services/account.service";
 
 const ChangePassword = () => {
-  return (
-    <Form>
-      <Container className="register">
-        <Row className="register-box">
-          <Row className='mt-5'>
-            <h1>Change Password</h1>
-          </Row>
-          <Row>
-            <FormGroup>
-              <Label className="mt-3" for="old-password">
-                Old Password<span className="required-star">*</span>
-              </Label>
-              <Input
-                required={true}
-                className="input"
-                id="old-password"
-                type="password"
-              />
+    const nevigate = useNavigate();
+    const dispatch = useDispatch();
+    const [invalid, setinvalid] = useState({ len: false, match: false });
 
-              <Label className="mt-3" for="new-password">
-                New Password<span className="required-star">*</span>
-              </Label>
-              <Input
-                required={true}
-                className="input"
-                id="new-password"
-                type="password"
-              />
-            </FormGroup>
-          </Row>
-          <Row className="justify-evenly mt-4">
-            <Col>
-              <Link className='link-btn-text' to='/my-profile'>
-                <Button className="edit-save-btn" type="submit" color="primary" size="lg">
-                  Save
-                </Button>
-              </Link>
-            </Col>
-            <Col>
-              <Link className='link-btn-text' to='/my-profile'>
-                <Button className='edit-cancel-btn' color='secondary' size='lg'>
-                  Cancel
-                </Button>
-              </Link>
-            </Col>
-          </Row>
-        </Row>
-      </Container>
-    </Form>
-  )
-}
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setinvalid({ len: false, match: false });
+        console.log(e.target.old_password.value);
 
-export default ChangePassword
+        if (e.target.old_password.value.length < 8) {
+            setinvalid({ ...invalid, len: true });
+        } else if (e.target.old_password.value != e.target.new_password.value) {
+            setinvalid({ ...invalid, match: true });
+        } else {
+            const body = {
+                password: e.target.old_password.value,
+            };
+
+            dispatch(startLoading());
+            chagenPassword(body)
+                .then((response) => {
+                    dispatch(stopLoading());
+                    emitSuccess("Your password has been changed!");
+                    nevigate("/my-profile");
+                })
+                .catch((err) => {
+                    dispatch(stopLoading());
+                    emitError("This password has already used!")
+                });
+        }
+    };
+
+    return (
+        <Form onSubmit={(e) => handleSubmit(e)}>
+            <Container className="register">
+                <Row className="register-box">
+                    <Row className="mt-5">
+                        <h1>Change Password</h1>
+                    </Row>
+                    <Row>
+                        <FormGroup>
+                            <Label className="mt-3" for="old_password">
+                                Old Password
+                                <span className="required-star">*</span>
+                            </Label>
+                            <Input
+                                required={true}
+                                className="input"
+                                id="old_password"
+                                type="password"
+                                invalid={invalid.len}
+                            />
+                            <FormFeedback>
+                                Your password must contain at least 8 characters
+                            </FormFeedback>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label className="mt-3" for="new_password">
+                                New Password
+                                <span className="required-star">*</span>
+                            </Label>
+                            <Input
+                                required={true}
+                                className="input"
+                                id="new_password"
+                                type="password"
+                                invalid={invalid.match}
+                            />
+                            <FormFeedback>
+                                Your password doesn't match with above
+                            </FormFeedback>
+                        </FormGroup>
+                    </Row>
+                    <Row className="justify-evenly mt-4">
+                        <Col>
+                            <Button
+                                className="edit-save-btn"
+                                type="submit"
+                                color="primary"
+                                size="lg"
+                            >
+                                Save
+                            </Button>
+                        </Col>
+                        <Col>
+                            <Button
+                                className="edit-cancel-btn"
+                                type="submit"
+                                color="secondary"
+                                size="lg"
+                                onClick={() => nevigate("/my-profile")}
+                            >
+                                Cancel
+                            </Button>
+                        </Col>
+                    </Row>
+                </Row>
+            </Container>
+        </Form>
+    );
+};
+
+export default ChangePassword;
