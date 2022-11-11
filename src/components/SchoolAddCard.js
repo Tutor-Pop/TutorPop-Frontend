d26fe5cb1cee71906796e7b153d0ecb72dd6e977
vknowtheyknow <56762841+vknowtheyknow@users.noreply.github.com>
2022-11-11 20:53:15 +0700

    link myschool

--- src/components/SchoolAddCard.js
+++ src/components/SchoolAddCard.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+ 
 const SchoolAddCard = () => {
   return (
     <span  className='schoolcard' style={{"display": "block"}}>
-    <Link to='/school-manage/:schoolid'>
+    <Link to='/school-register'>
     <div className='imgaddschool'>
                 <img src={require("../img/add.png")}/>
             </div>
     </Link>
   </span>
-    
+   
   )
 }
-
+ 
 export default SchoolAddCard
--- src/services/personal.service.js
+++ src/services/personal.service.js
@@ -1,26 +1,27 @@
 const axios = require('axios')
 const { BACKEND_URL } = require('../constants/service.constant')
-
+ 
 export async function getAllReservations(account_id){
     return axios.get(`${BACKEND_URL}/api/accounts/${account_id}/reservations`)
 }
-
+ 
 export async function getReserve(){
-
+ 
 }
-
+ 
 export async function getTeachings(){
-
+ 
 }
-
+ 
 export async function getTime(){
-
+ 
 }
-
-export async function getSchoolMembers(){
-
+ 
+export async function getSchoolMembers(account_id){
+    return axios.get(`${BACKEND_URL}/api/${account_id}/schools`);
 }
-
-export async function getSchoolOwner(){
-
+ 
+export async function getSchoolOwner(account_id){
+    return axios.get(`${BACKEND_URL}/api/${account_id}/owners`);
+   
 }
