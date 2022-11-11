7ec7041b6132706d372d02487baa0a2933d224db
PF-Poppy <Puifaiiwimonmanee@gmail.com>
2022-11-12 01:14:42 +0700

    addservicecource

--- src/services/course.service.js
+++ src/services/course.service.js
@@ -1,8 +1,8 @@
 const axios = require('axios')
 const { BACKEND_URL } = require('../constants/service.constant')
 
-export async function getCourse(school_id,course_id){
-    return axios.get(`${BACKEND_URL}/api/schools/${school_id}/courses/${course_id}`)
+export async function getCourse(course_id){
+    return axios.get(`${BACKEND_URL}/api/courses/${course_id}`)
 }
 
 export async function createCourse(school_id,body){
@@ -13,24 +13,28 @@ export async function updateCourse(school_id,course_id,body){
     return axios.put(`${BACKEND_URL}/api/schools/${school_id}/courses/${course_id}`,body)
 }
 
-export async function deleteCourse(school_id,course_id){
-    return axios.get(`${BACKEND_URL}/api/schools/${school_id}/courses/${course_id}`)
+export async function deleteCourse(course_id){
+    return axios.get(`${BACKEND_URL}/api/courses/${course_id}`)
 }
 
-export async function getStudents(school_id,course_id){
-    return axios.get(`${BACKEND_URL}/api/schools/${school_id}/courses/${course_id}/students`)
+export async function getStudents(course_id){
+    return axios.get(`${BACKEND_URL}/api/courses/${course_id}/students`)
 }
 
-export async function getTeachers(school_id,course_id){
-    return axios.get(`${BACKEND_URL}/api/schools/${school_id}/courses/${course_id}/teachers`)
+export async function getTeachers(course_id){
+    return axios.get(`${BACKEND_URL}/api/courses/${course_id}/teachers`)
 }
 
 export async function updateTeachers(school_id,course_id,body){
     return axios.put(`${BACKEND_URL}/api/schools/${school_id}/courses/${course_id}/teachers`,body)
 }
 
-export async function deleteTeacher(school_id,course_id){
-    return axios.delete(`${BACKEND_URL}/api/schools/${school_id}/courses/${course_id}/teachers`)
+export async function deleteTeacher(course_id,body){
+    return axios.delete(`${BACKEND_URL}/api/courses/${course_id}/teachers`,body)
+}
+
+export async function putTeachers(course_id,body) {
+    return axios.get(`${BACKEND_URL}/api/courses/${course_id}/teachers`,body)
 }
 
 export async function getTeachingCourses(account_id) {
@@ -43,4 +47,28 @@ export async function getStudyCourses(account_id) {
 
 export async function getAllCourses(school_id) {
     return axios.get(`${BACKEND_URL}/api/schools/${school_id}/courses`)
+}
+
+export async function getsinglecoursewithdetail(course_id) {
+    return axios.get(`${BACKEND_URL}/api/courses/${course_id}/details`)
+}
+
+export async function putCourse(course_id,body) {
+    return axios.get(`${BACKEND_URL}/api/courses/${course_id}`,body)
+}
+
+export async function getallcoursespopulate() {
+    return axios.get(`${BACKEND_URL}/api/courses/populate`)
+}
+
+export async function uploadpaymentmethodpictureforcourse(course_id,body) {
+    return axios.get(`${BACKEND_URL}/api/courses/${course_id}/upload_payment`,body)
+}
+
+export async function uploadpaymentmethodpictureforcourse(course_id,body) {
+    return axios.get(`${BACKEND_URL}/api/courses/${course_id}/upload_payment`,body)
+}
+
+export async function uploadcoursepicture(course_id,body) {
+    return axios.get(`${BACKEND_URL}/api/courses/${course_id}/upload_poster`,body)
 }
