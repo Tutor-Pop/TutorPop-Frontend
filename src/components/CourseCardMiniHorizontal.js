89aec36003538a330958d34b4258e8efc61a7c50
KanonKC <kanonkce@gmail.com>
2022-11-11 05:18:50 -0800

    Unfinished Scroll

--- src/components/CourseCardMiniHorizontal.js
+++ src/components/CourseCardMiniHorizontal.js
@@ -1,11 +1,60 @@
 import React from 'react'
+import { Col, Row } from 'reactstrap'
+
+const PROFILE = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Solid_blue.svg/800px-Solid_blue.svg.png"
 
 const CourseCardMiniHorizontal = () => {
   return (
-    <div className='ccmh'>
-        <h1>asd</h1>
+    <div className='course-card-mini-horizontal mx-2 d-flex'>
+      <img src={PROFILE} width={118} height={118}></img>
+
+      <div className='course-card-mini-horizontal-description ml-2'>
+        <div className='pt-1'>
+
+          <p className='font-bold'>Course Name</p>
+          <p>Teacher</p>
+          <p>School Name</p>
+          <p>200 THB</p>
+        </div>
+      </div>
+
     </div>
   )
 }
 
 export default CourseCardMiniHorizontal
+
+// import * as React from 'react';
+// import { useTheme } from '@mui/material/styles';
+// import Box from '@mui/material/Box';
+// import Card from '@mui/material/Card';
+// import CardContent from '@mui/material/CardContent';
+// import CardMedia from '@mui/material/CardMedia';
+// import Typography from '@mui/material/Typography';
+
+// export default function CourseCardMiniHorizontal() {
+//   const theme = useTheme();
+
+//   return (
+//     <Card sx={{ display: 'flex' }}>
+//       <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+//         <CardContent sx={{ flex: '1 0 auto' }}>
+//           <Typography component="div" variant="h5">
+//             Live From Space
+//           </Typography>
+//           <Typography variant="subtitle1" color="text.secondary" component="div">
+//             Mac Miller
+//           </Typography>
+//         </CardContent>
+//         <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
+//         </Box>
+//       </Box>
+//       <CardMedia
+//         component="img"
+//         sx={{ width: 151 }}
+//         image="/static/images/cards/live-from-space.jpg"
+//         alt="Live from space album cover"
+//       />
+//     </Card>
+//   );
+// }
--- src/views/SearchPage.js
+++ src/views/SearchPage.js
@@ -9,7 +9,11 @@ import CourseCardMiniHorizontal from '../components/CourseCardMiniHorizontal'
 const SearchPage = () => {
 
     const dispatch = useDispatch()
-    const [isSelectRight,setisSelectRight] = useState(0)
+    const [swap, setswap] = useState(0)
+
+    const toggle = () => {
+        setswap(swap == 0 ? 1 : 0)
+    }
 
     const handleClick = () => {
         const payload = {
@@ -20,11 +24,40 @@ const SearchPage = () => {
     }
 
     return (
-        <div className='pt-20'>
-            <div>
+        <div className='pt-20 mx-20'>
+            <div className='mx-auto w-1/4 my-3'>
                 <SearchBar />
             </div>
-            
+            <CourseContainer
+                leftText="Course"
+                rightText="School"
+                isSelectRight={swap}
+                onClick={toggle}
+            >
+                {
+                    swap == 0 ? <>
+                        {/* Course */}
+                        <h3>Education</h3>
+                        <div className='search-scroll'>
+                        <CourseCardMiniHorizontal />
+                        <CourseCardMiniHorizontal />
+                        <CourseCardMiniHorizontal />
+                        <CourseCardMiniHorizontal />
+                    
+
+                        </div>
+                        <h3>Music</h3>
+                        <div className='search-scroll'>
+                        <CourseCardMiniHorizontal />
+                        <CourseCardMiniHorizontal />
+                        <CourseCardMiniHorizontal />
+                        </div>
+                    </> : <>
+                        {/* School */}
+                        <h2>SKDJ</h2>
+                    </>
+                }
+            </CourseContainer>
         </div>
     )
 }
