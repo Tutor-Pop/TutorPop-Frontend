f255e76848a433d926a8aef507c8a770220b3d6e
vknowtheyknow <56762841+vknowtheyknow@users.noreply.github.com>
2022-11-12 07:07:27 +0700

    link success

--- src/views/MySchool.js
+++ src/views/MySchool.js
@@ -2,7 +2,7 @@
 import React, { useState } from 'react'
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import { Col, Container, Row } from 'reactstrap'
 import SchoolAddCard from '../components/SchoolAddCard'
 import SchoolCard from '../components/SchoolCard'
@@ -17,9 +17,9 @@ const MySchool = () => {
  
  
   useEffect(() => {
-    dispatch(startLoading())
+    
     getSchoolOwner(localStorage.getItem("account_id")).then((res)=>{
-      dispatch(stopLoading())
+      
       console.log(res.data);
       setmyAllSchool(res.data.schools)
     }
@@ -36,14 +36,15 @@ const MySchool = () => {
         <Container className='SchoolBox'>
             <Row md={3} xs={2} xl={4} xxl={5}>
               {myallSchool.map((owner) => (
-                <Col className='myschool-img border'>
+                <Link to={`/school-manage/${owner.school_id}`}>
+                <Col className='myschool-img border' >
                   <SchoolCard
                     school_id={owner.school_id}
                     school_name={owner.name}
                     logo_url={owner.logo_pic}
                     status={owner.status}
                   />
-                </Col>
+                </Col></Link>
               ))}
               <Col className='myschool-img border'><SchoolAddCard/></Col>
             </Row>
